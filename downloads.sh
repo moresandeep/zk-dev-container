73560bac0c1d161e9ac43268767aa7f60e7ff505
Scott Clasen <scott@heroku.com>
2013-10-22 21:38:26 -0700

    downloads

--- downloads.sh
+++ downloads.sh
@@ -2,6 +2,10 @@
 
 #run before docker build, so if you are deving you dont have to keep redownloading stuff
 
-curl -O  http://www.gtlib.gatech.edu/pub/apache/zookeeper/zookeeper-3.4.5/zookeeper-3.4.5.tar.gz 
+curl -O http://mirrors.sonic.net/apache/zookeeper/zookeeper-3.4.5/zookeeper-3.4.5.tar.gz 
 
 curl -O http://ringmaster-exhibitor.s3.amazonaws.com/exhibitor-standalone-1.4.7.jar 
+
+curl -O https://raw.github.com/jpetazzo/pipework/master/pipework
+
+chmod +x pipework
